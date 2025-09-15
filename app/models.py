afe8b9ac84cb006788ba539afb20e023050b7804
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2025-09-15 18:30:12 +0200

    updated models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 from typing import Optional
 
-class User(BaseModel):
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str
+    password: str
 
-class Item(BaseModel):
+class Item(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     description: Optional[str] = None
     price: float
