3e8fdffe3a455a60d6f9fd4a5a36aa4933e22dc9
Wallace ESPINDOLA <wallace.espindola@gmail.com>
2025-09-16 19:54:07 +0200

    fix pipeline

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from sqlmodel import Field, SQLModel
 
 
-class User(SQLModel, table=True):  # type: ignore[call-arg]
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     email: str
     password: str
 
 
-class Item(SQLModel, table=True):  # type: ignore[call-arg]
+class Item(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     description: str | None = None
